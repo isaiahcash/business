f752d6f70e5a5450440615191c705e9710abc025
Isaiah Cash <isaiahcash@gmail.com>
2021-05-21 19:46:43 +0000

    Minor updates.

--- config.php
+++ config.php
@@ -2,6 +2,9 @@
 session_start();
 $_SESSION['max_file_size'] =  10000000000000000000;
 date_default_timezone_set("America/New_York");
+
+error_reporting(0);
+
 require_once('includes/functions.php');
 require_once('includes/mysql.php');
 require_once('includes/credentials.php');
